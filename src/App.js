42515f2d92ad825fff9281a463f819234fe60104
NeoSelf1 <neoself1105@gmail.com>
2023-09-21 16:04:50 +0900

    minorFix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Product from "./routes/Product";
 import AboutUs from "./routes/AboutUs";
-// import {Helmet} from './react-helmet'
 
 function App() {
   return (
